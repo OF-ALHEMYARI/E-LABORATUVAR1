e7a19b758648aa2cb15841889854fe0f3ce32a4a
OF-ALHEMYARI <omarf.alhemyri10@gmail.com>
2024-12-25 21:32:33 +0300

    Create new expo project add gluestack and tailwind css

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Tab One",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
